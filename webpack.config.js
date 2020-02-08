4c0ad4d4fdccc3556d20429fd3a60e09568f3873
Jhon-Mosk <jhon-mosk@yandex.ru>
2020-02-08 10:37:55 +0300

    install babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,13 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),     //путь к выходной папке
         publicPath: '/dist'        //папка нужно для работы devServer
     },
+    module: {
+        rules: [{
+            test: /\.js$/,              //какие файлы обрабатываем
+            loader: 'babel-loader',     //через что обрабатываем файлы
+            exclude: '/node_modules/'   //исключаем из обработки    
+        }]
+    },
     devServer: {
         overlay: true       //параметр: показ ошибок на экране браузера
     }
