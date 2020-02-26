933b29f2c2749d738de76cf9943542e159bb6d4d
Jhon-Mosk <jhon-mosk@yandex.ru>
2020-02-26 12:16:49 +0300

    add function to pug processing

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 const PATHS = {
     src: path.join(__dirname, '../src'),
@@ -10,8 +11,36 @@ const PATHS = {
     assets: 'assets/'
 }
 
-const PAGES_DIR = `${PATHS.src}/pages/`              //откуда берутся страницы в паге
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+// const PAGES_DIR = `${PATHS.src}/pages/`              //откуда берутся страницы в паге
+// const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+// var results = [];
+function fromDir(startPath,filter){
+    
+    var results = [];
+
+    if (!fs.existsSync(startPath)){
+        console.log("no dir ",startPath);
+        return;
+    }
+
+    var files=fs.readdirSync(startPath);
+    for(var i=0;i<files.length;i++){
+        var filename=path.join(startPath,files[i]);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory()){
+            results = results.concat(fromDir(filename,filter)); //recurse
+        }
+        else if (filename.indexOf(filter)>=0) {
+            console.log('-- found: ',filename);
+            results.push(filename);
+        };
+    };
+    return results;
+};
+
+// fromDir(`${PATHS.src}`,'.pug');
+
+let pugPages = fromDir(`${PATHS.src}`,'.pug');
 
 module.exports = {
 
@@ -85,6 +114,7 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: `${PATHS.assets}css/[name].[hash].css`
         }),
+        new CleanWebpackPlugin (),
         // new HtmlWebpackPlugin({
         //     hash: false,                         //выключает хэши
         //     template: `${PATHS.src}/index.html`,
@@ -97,8 +127,9 @@ module.exports = {
             { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}/fonts` }
         ]),
 
-        ...PAGES.map(page => new HtmlWebpackPlugin({
-            template: `${PAGES_DIR}/${page}`,           //на входе паг
+        ...pugPages.map(page => new HtmlWebpackPlugin({
+            template: `${page}`,           //на входе паг
+            // filename: `${page.replace(/\.pug/,'.html').replace(/\/src\//,'/dist/')}`
             filename: `./${page.replace(/\.pug/,'.html')}`              //на выходе хтмл
         }))
     ]
